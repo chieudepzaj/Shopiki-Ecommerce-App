b9ffdebfa8db3803c421ccd6fa23d49c6f2c2996
chieudepzaj <chieumu1999@gmail.com>
2021-11-29 13:58:18 +0700

    admin layout/ add product ...

---
+++ app/src/main/java/com/example/shopiki/models/Constant.java
@@ -0,0 +1,16 @@
+package com.example.shopiki.models;
+
+public class Constant {
+
+    //product categories
+    public static final String[] option = {
+            "Thiết Bị Số",
+            "Bách Hóa",
+            "Thời Trang Nam",
+            "Điện Tử",
+            "Điện Thoại",
+            "Thời Trang Nữ",
+            "Giày Nam",
+            "Sách"
+    };
+}
